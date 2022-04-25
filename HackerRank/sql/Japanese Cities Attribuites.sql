052265e104316bef3d3d3bb90496a725c5fb2d4f
Yehia hossam <yehiahosam34@gmail.com>
2022-04-25 18:08:19 +0200

    japaneses cities attribuites

---
+++ HackerRank/sql/Japanese Cities Attribuites.sql
@@ -0,0 +1,7 @@
+/*
+Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+The CITY table is described as follows:
+CITY.jpg
+*/
+select * from CITY where COUNTRYCODE = 'JPN';
