c12ede79f8945c7b7356eb98f12c2fd36cff18cf
Yehia hossam <yehiahosam34@gmail.com>
2022-04-25 18:05:23 +0200

    sql 2nd problem

---
+++ HackerRank/sql/Revising the Select Query lI.sql
@@ -0,0 +1,8 @@
+/*
+Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+
+The CITY table is described as follows:
+
+*/
+
+select NAME from CITY where POPULATION >120000 and COUNTRYCODE = 'USA';
