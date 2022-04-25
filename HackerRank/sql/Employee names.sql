8d133f223448d4a16c901f81b0d30b158b909bb8
Yehia hossam <yehiahosam34@gmail.com>
2022-04-26 00:58:47 +0200

    Employee names

---
+++ HackerRank/sql/Employee names.sql
@@ -0,0 +1,9 @@
+/*
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+Input Format
+
+The Employee table containing employee data for a company is described as follows:
+
+*/
+select name from Employee order by name
