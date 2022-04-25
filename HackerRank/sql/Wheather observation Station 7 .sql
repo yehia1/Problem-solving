66e0fa831b591cf8b41f7c22b8ecd1538c4c027b
Yehia hossam <yehiahosam34@gmail.com>
2022-04-25 22:03:46 +0200

    wheather observation station 7

---
+++ HackerRank/sql/Wheather observation Station 7 .sql
@@ -0,0 +1,11 @@
+/*
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+Station.jpg
+*/
+
+select distinct CITY from Station where CITY like '%[ieaou]'
