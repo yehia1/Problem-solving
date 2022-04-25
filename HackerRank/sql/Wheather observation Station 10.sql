d5ccd7cbe8d5924ce83713df8f0657fffefe3b88
Yehia hossam <yehiahosam34@gmail.com>
2022-04-25 22:35:21 +0200

    wheather observation station 10

---
+++ HackerRank/sql/Wheather observation Station 10.sql
@@ -0,0 +1,12 @@
+/*
+Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+Station.jpg
+*/
+
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY like '%[^ieauo]'
