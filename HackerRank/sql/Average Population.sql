7c537d39ea2c3fe3a1e9dcfdd2090f4af1382782
Yehia hossam <yehiahosam34@gmail.com>
2022-04-27 02:29:22 +0200

    average rounded population

---
+++ HackerRank/sql/Average Population.sql
@@ -0,0 +1,8 @@
+/*
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+
+Input Format
+
+The CITY table is described as follows:
+*/
+select round(avg(population),0)from City
