0777b22047704be9d62d3e92120c54123b645798
Yehia Hossam <yehiahosam34@gmail.com>
2022-11-23 20:59:20 +0200

    Random numbers

---
+++ LeetCode/Random/FindMedianSortedArrays.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums3 = nums1 + nums2
+        nums3 = sorted(nums3)
+        length = len(nums3)-1 
+        if(length % 2 == 1):
+            return (nums3[length//2]+nums3[length//2+1])/2
+        else :
+            return nums3[length // 2]
