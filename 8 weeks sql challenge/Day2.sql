c919682d780183850514755612d8e80a8851697a
Yehia Hossam <yehiahosam34@gmail.com>
2022-12-20 00:58:13 +0200

    day 2 other solution

--- 8 weeks sql challenge/Day2.sql
+++ 8 weeks sql challenge/Day2.sql
@@ -19,3 +19,21 @@ inner join dannys_diner.menu m
 on s.product_id = m.product_id
 group by m.product_name
 limit 1 
+
+---- What is the most purchased item on the menu and how many times was it purchased by all customers?
+select s.customer_id,product_name as maximum_product,count(s.product_id)
+	FROM dannys_diner.menu m 
+	inner join dannys_diner.sales s
+	on s.product_id = m.product_id 
+    group by s.customer_id,product_name
+    having m.product_name = (SELECT product_name
+      FROM dannys_diner.menu m 
+      inner join dannys_diner.sales s
+      on s.product_id = m.product_id
+      group by product_name
+      having count(s.product_id) = (SELECT max(counts) from 
+      (SELECT product_name, count(s.product_id) counts
+          FROM dannys_diner.menu m 
+          inner join dannys_diner.sales s
+          on s.product_id = m.product_id
+          group by product_name) as sub));
