a1a9e8445df81c774c2a54df58d877fe71b047a2
Yehia Hossam <yehiahosam34@gmail.com>
2022-12-23 16:26:06 +0200

    day2 final

--- 8 weeks sql challenge/Case Study #2 - Pizza Runner/Day2.sql
+++ 8 weeks sql challenge/Case Study #2 - Pizza Runner/Day2.sql
@@ -24,7 +24,22 @@ SELECT customer_id,
     order by customer_id;
 
 --How many pizzas were delivered that had both exclusions and extras?
+SELECT sum(case 
+    	when exclusions is not null and extras is not null then 1
+        else 0 END) as Both_extras_and_exclusions_del
+    from cleaned_customer c
+    inner join cleaned_runner r 
+    on c.order_id = r.order_id
+    where distance != 0;
 
 -- What was the total volume of pizzas ordered for each hour of the day?
+Select EXTRACT(HOUR FROM order_time)as hours,count(order_id) as total_orders
+	From cleaned_customer
+    group by hours
+    order by hours;
 
 -- What was the volume of orders for each day of the week?
+Select to_char(order_time + INTERVAL '2 day','Day') as day_of_week, count(order_id) as total_orders -- adding 2 to the day because it's starts from monday
+	From cleaned_customer
+    group by day_of_week
+    order by day_of_week;
