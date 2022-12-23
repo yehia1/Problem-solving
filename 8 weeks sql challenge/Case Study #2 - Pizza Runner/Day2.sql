5798ac18e642ef3803eff1a8ecf544aacfb3b0e8
Yehia Hossam <yehiahosam34@gmail.com>
2022-12-23 15:46:28 +0200

    day 2 2nd problem

--- 8 weeks sql challenge/Case Study #2 - Pizza Runner/Day2.sql
+++ 8 weeks sql challenge/Case Study #2 - Pizza Runner/Day2.sql
@@ -3,13 +3,25 @@ Set
     
 -- What was the maximum number of pizzas delivered in a single order?
 Select order_id,count(pizza_id) as number_of_pizzas
-From customer_orders c
+From cleaned_customer c
 group by order_id
 order by number_of_pizzas desc
-Limit 1
+Limit 1;
 
 -- For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
-
+SELECT customer_id,
+	sum(case 
+    	when exclusions is not null or extras is not null then 1
+        else 0 END) as at_least_1_change,
+    	sum(case 
+    	when exclusions is null and extras is null then 1
+        else 0 END) as No_Change
+    from cleaned_customer c
+    inner join cleaned_runner r 
+    on c.order_id = r.order_id
+    where distance != 0 
+    group by customer_id
+    order by customer_id;
 
 --How many pizzas were delivered that had both exclusions and extras?
 
