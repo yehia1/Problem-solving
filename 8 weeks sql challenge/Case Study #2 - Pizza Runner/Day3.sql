f6f55d344b374de4e2ed9e009444d277e1961be7
Yehia Hossam <yehiahosam34@gmail.com>
2022-12-25 00:37:32 +0200

    day 3 all

--- 8 weeks sql challenge/Case Study #2 - Pizza Runner/Day3.sql
+++ 8 weeks sql challenge/Case Study #2 - Pizza Runner/Day3.sql
@@ -26,5 +26,52 @@ from cleaned_runner r
 inner join cleaned_customer c
 on c.order_id = r.order_id
 group by runner_id
-order by runner_id
-      
+order by runner_id;
+
+-- Is there any relationship between the number of pizzas and how long the order takes to prepare?
+Select c.order_id,count(pizza_id) as number_of_pizzas,
+Round(avg(DATE_PART('minute', pickup_time - order_time))) as minutes_delay
+from cleaned_runner r
+inner join cleaned_customer c
+on c.order_id = r.order_id
+where distance is not NULL
+group by c.order_id
+ORDER BY number_of_pizzas desc;
+
+
+-- What was the average distance travelled for each customer?
+Select customer_id,round(avg(distance)) as distance
+from cleaned_runner r
+inner join cleaned_customer c
+on c.order_id = r.order_id
+group by customer_id
+order by customer_id;
+
+--What was the difference between the longest and shortest delivery times for all orders?
+Select (max(duration) - min(duration)) as max_diff
+from cleaned_runner;
+
+-- What was the average speed for each runner for each delivery and do you notice any trend for these values?
+Select runner_id,
+	Concat(Round(cast(avg(distance / (duration/60)) as numeric),1),' KM\H') as speed
+    from cleaned_runner
+    where distance is not Null
+    group by runner_id
+    order by runner_id;
+    
+-- What is the successful delivery percentage for each runner?
+Select runner_id,
+Concat(Round(success / (success+failures)*100),'%') 
+as successful_percentage from
+(Select runner_id,
+    cast(sum(case
+      when distance is null then 1
+      else 0
+      end )as numeric) as failures,
+      cast(sum(case
+      when distance is not null then 1
+      else 0
+      end) as numeric)as success
+	From cleaned_runner
+    group by runner_id) as sub
+    order by runner_id
