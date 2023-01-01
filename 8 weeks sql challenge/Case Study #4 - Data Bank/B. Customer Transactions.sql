4a1abbc2b553f108b8739e0e04379f81321a1301
Yehia Hossam <yehiahosam34@gmail.com>
2023-01-01 14:09:28 +0200

    Data Bank

---
+++ 8 weeks sql challenge/Case Study #4 - Data Bank/B. Customer Transactions.sql
@@ -0,0 +1,33 @@
+SET search_path = data_bank;
+
+-- What is the unique count and total amount for each transaction type?
+Select txn_type,count(distinct txn_amount),sum(txn_amount)
+from customer_transactions
+Group by txn_type;
+
+-- What is the average total historical deposit counts and amounts for all customers?
+Select count(*) count_of_deposits,
+	Round(avg(txn_amount),2) as avergae_amount
+from customer_transactions
+where txn_type = 'deposit';
+
+--For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
+WITH monthly_transactions AS (
+  SELECT 
+    customer_id, 
+    Extract(month from txn_date) AS month,
+    SUM(CASE WHEN txn_type = 'deposit' THEN 0 ELSE 1 END) AS deposit_count,
+    SUM(CASE WHEN txn_type = 'purchase' THEN 0 ELSE 1 END) AS purchase_count,
+    SUM(CASE WHEN txn_type = 'withdrawal' THEN 1 ELSE 0 END) AS withdrawal_count
+  FROM customer_transactions
+  GROUP BY customer_id, month
+ )
+
+SELECT
+  month,
+  COUNT(DISTINCT customer_id) AS customer_count
+FROM monthly_transactions
+WHERE deposit_count > 1 
+  AND (purchase_count > 1 OR withdrawal_count > 1)
+GROUP BY month
+ORDER BY month;
