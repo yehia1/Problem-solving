f3d3c6a614d625beafd05a3815eaac92c474c863
Yehia Hossam <yehiahosam34@gmail.com>
2023-01-04 00:31:31 +0200

    a.Data cleansing step

--- 8 weeks sql challenge/Case Study #5 - Data Mart/1. Data Cleansing Steps.sql
+++ 8 weeks sql challenge/Case Study #5 - Data Mart/1. Data Cleansing Steps.sql
@@ -6,7 +6,26 @@ SELECT
 	To_date(week_date, 'dd/mm/yy') as date,
   	Extract('week' from To_date(week_date, 'dd/mm/yy')) as week,
   	Extract('month' from To_date(week_date, 'dd/mm/yy')) as month,
-  	Extract('year' from To_date(week_date, 'dd/mm/yy')) as year
+  	Extract('year' from To_date(week_date, 'dd/mm/yy')) as year,
+  region,
+  platform,
+  segment,
+case 
+  	when right(segment,1) = '1' then 'Young Adults'
+  	when right(segment,1) = '2' then 'Middle Aged'
+  	when right(segment,1) in ('3','4') then 'Middle Aged'
+  	else 'unknown'
+  	end as age_band,
+case
+  	when left(segment,1) = 'C' then 'Couples'
+  	when left(segment,1) = 'F' then 'Families'
+  	else 'unknown'
+  	end as demographic,
+  customer_type,
+  transactions,
+  sales,
+Round(sales :: Numeric /transactions :: Numeric ,2)
+  as avg_transaction
 FROM weekly_sales);
   
 Select * from clean_weekly_sales
