24d510df58b98d8aef7eec0100b26c8a43b019e6
Yehia Hossam <yehiahosam34@gmail.com>
2023-01-03 14:38:09 +0200

    Case study 5 #2

--- 8 weeks sql challenge/Case Study #5 - Data Mart/1. Data Cleansing Steps.sql
+++ 8 weeks sql challenge/Case Study #5 - Data Mart/1. Data Cleansing Steps.sql
@@ -3,7 +3,10 @@ SET search_path = data_mart;
 DROP TABLE IF EXISTS clean_weekly_sales;
 CREATE TEMP TABLE clean_weekly_sales AS (
 SELECT
-	To_date(week_date, 'dd/mm/yy')
+	To_date(week_date, 'dd/mm/yy') as date,
+  	Extract('week' from To_date(week_date, 'dd/mm/yy')) as week,
+  	Extract('month' from To_date(week_date, 'dd/mm/yy')) as month,
+  	Extract('year' from To_date(week_date, 'dd/mm/yy')) as year
 FROM weekly_sales);
   
 Select * from clean_weekly_sales
