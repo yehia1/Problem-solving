31c339990d829bbaa9f8a9cd16fe1a735182fc70
Yehia Hossam <yehiahosam34@gmail.com>
2023-01-20 18:51:40 +0200

    3 categories aggregations

--- 8 weeks sql challenge/Case Study #6 - Clique Bait/3.Product_Funnel_Analysis.sql
+++ 8 weeks sql challenge/Case Study #6 - Clique Bait/3.Product_Funnel_Analysis.sql
@@ -30,4 +30,33 @@ where product_category is not null
 group by page_name;
 
 
-Select * from Products_aggregations
+-- create table which further aggregates the data for each Category.
+Select product_category,
+sum(case 
+	when event_type = 1 then 1 end)as views,
+sum(case 
+	when event_type = 2 then 1 end)as added_to_cart,
+sum(case 
+    	when event_type = 2 and 
+    visit_id in ( SELECT
+          distinct visit_id
+        FROM
+          events AS ee
+        WHERE
+          event_type = 3) then 1 end) as purchases,
+sum(case 
+    	when event_type = 2 and 
+    visit_id not in ( SELECT
+          distinct visit_id
+        FROM
+          events AS ee
+        WHERE
+          event_type = 3) then 1 end) as abandoned 
+INTO Category_aggregations
+from page_hierarchy p
+inner join events e
+on e.page_id = p.page_id
+where product_category is not null
+group by product_category;
+
+Select * from Category_aggregations
