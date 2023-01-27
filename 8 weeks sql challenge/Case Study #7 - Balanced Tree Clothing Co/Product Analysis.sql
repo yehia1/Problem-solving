d8b536a91e92d2c520b858fef8a3f3b914da60a9
Yehia Hossam <yehiahosam34@gmail.com>
2023-01-27 13:54:02 +0200

    Product analysis first 3 problems

---
+++ 8 weeks sql challenge/Case Study #7 - Balanced Tree Clothing Co/Product Analysis.sql
@@ -0,0 +1,35 @@
+set search_path = balanced_tree;
+
+--What are the top 3 products by total revenue before discount?
+Select distinct product_name,s.price * qty as revenue 
+from sales s
+inner join product_details pd
+on pd.product_id = s.prod_id
+order by 2 desc
+limit 3;
+
+--What is the total quantity, revenue and discount for each segment?
+Select segment_name,sum(qty) quantity
+		,sum(s.price * qty *(s.price * (discount :: Numeric/100)))revenue,
+        sum(s.price * (discount :: Numeric/100))discounts
+from sales s
+inner join product_details pd
+on pd.product_id = s.prod_id
+group by segment_name;
+
+--What is the top selling product for each segment?
+with segment_product_revenue as (
+Select segment_name,product_name,
+	round(sum(s.price * qty *(s.price * (discount :: Numeric/100))))revenue,
+	rank() over(partition by segment_name order by round(sum(s.price * qty *(s.price * (discount :: Numeric/100)))) desc) as rank
+from sales s
+inner join product_details pd
+on pd.product_id = s.prod_id
+group by segment_name,product_name)
+
+Select segment_name,product_name,revenue
+from segment_product_revenue
+where rank = 1
+
+
+            
