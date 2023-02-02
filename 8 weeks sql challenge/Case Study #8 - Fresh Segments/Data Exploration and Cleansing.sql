eac599a41e683a4e2e3a29d7292044f1183f192c
Yehia Hossam <yehiahosam34@gmail.com>
2023-02-02 15:37:32 +0200

    data exploration finished

--- 8 weeks sql challenge/Case Study #8 - Fresh Segments/Data Exploration and Cleansing.sql
+++ 8 weeks sql challenge/Case Study #8 - Fresh Segments/Data Exploration and Cleansing.sql
@@ -11,16 +11,19 @@ group by 1
 order by month_year Nulls first;
 
 --How many interest_id values exist in the fresh_segments.interest_metrics table but not in the fresh_segments.interest_map table? What about the other way around?
-Select count(interest_id) from interest_metrics
+Select count(interest_id) as distincts from interest_metrics
 where 
 	interest_id :: int Not in
-    (Select id from interest_map);
-
+    (Select id from interest_map)
+union
 Select count(id) from interest_map
 where 
 	id Not in
     (Select interest_id :: int from interest_metrics
     where interest_id is not Null);
+
+--Summarise the id values in the fresh_segments.interest_map by its total record count in this table
+SELECT count(id) from interest_map;
 
 --What sort of table join should we perform for our analysis and why? Check your logic by checking the rows 
 --where interest_id = 21246 in your joined output and include all columns from fresh_segments.interest_metrics and all columns 
@@ -32,3 +35,9 @@ inner join interest_map im1
 on im1.id = im.interest_id :: Numeric
 where interest_id = '21246';
 
+--Are there any records in your joined table where the month_year value is before the created_at value from the fresh_segments.interest_map table? Do you think these values are valid and why?
+Select count(im.*) from
+interest_metrics im 
+inner join interest_map im1
+on im.interest_id :: int = im1.id
+where month_year < created_at
