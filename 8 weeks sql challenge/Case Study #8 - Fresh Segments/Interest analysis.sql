dc866cd8534e7179c9a236551e17b6f7990d7981
Yehia Hossam <yehiahosam34@gmail.com>
2023-02-06 15:28:34 +0200

    interest analysis 2

--- 8 weeks sql challenge/Case Study #8 - Fresh Segments/Interest analysis.sql
+++ 8 weeks sql challenge/Case Study #8 - Fresh Segments/Interest analysis.sql
@@ -13,3 +13,27 @@ where month_year in
 ( SELECT DISTINCT month_year FROM interest_metrics)
 group by 1
 having count(interest_name) = 14;
+
+
+--Using this same total_months measure - calculate the cumulative percentage of all records starting at 14 months - which total_months value passes the 90% cumulative percentage value?
+with counted_months as (
+SELECT
+      interest_id,
+      COUNT(interest_id) total_months,
+      ROW_NUMBER() OVER(
+        PARTITION BY COUNT(interest_id)
+        ORDER BY
+          COUNT(interest_id)
+      ) AS rank
+    FROM
+      interest_metrics AS im
+    GROUP BY
+      1
+    HAVING
+      COUNT(interest_id) > 0)
+
+select total_months,max(rank) as number_of_interests,round(100 * sum(max(rank)) over (order by total_months)
+/ SUM(MAX(rank)) OVER (),2) as cum_perc
+from counted_months
+group by total_months
+order by 3 desc
