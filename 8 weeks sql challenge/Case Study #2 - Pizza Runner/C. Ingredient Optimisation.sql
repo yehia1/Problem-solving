a831c93ca82f234ea1e612c39fc2a3ae76055f7e
Yehia Hossam <yehiahosam34@gmail.com>
2023-02-17 20:56:24 +0200

    ingredint 5th prob

--- 8 weeks sql challenge/Case Study #2 - Pizza Runner/C. Ingredient Optimisation.sql
+++ 8 weeks sql challenge/Case Study #2 - Pizza Runner/C. Ingredient Optimisation.sql
@@ -51,4 +51,64 @@ Select topping_name,count_of_extras
 From number_of_extras
 Where count_of_extras = (Select max(count_of_extras) From number_of_extras)
 
+-- Generate an order item for each record in the customers_orders table in the format of one of the following
+With pizza_topping_cte as(
+Select order_id,pizza_name,
+    Case
+    When exclusions Is Not null And t.topping_id IN
+    	(Select 
+    	UNNEST(STRING_TO_ARRAY(exclusions, ','):: int [])
+    	)
+    Then topping_name
+    End as exclusions,
+    Case
+    When extras Is Not null And t.topping_id IN
+    	(Select 
+    	UNNEST(STRING_TO_ARRAY(extras, ',') :: int [])
+    	)
+    Then topping_name
+    End as extras
+From pizza_toppings as t,
+cleaned_customer as c
+JOIN pizza_names as n 
+ON c.pizza_id = n.pizza_id
+GROUP BY c.order_id,pizza_name,
+exclusions,extras,topping_id,topping_name)
+
+Select order_id,
+	Concat(pizza_name,' ',
+	Case 
+	When Count(extras) > 0 Then '- Extra '
+	Else '' End,
+    String_agg(extras,', '),' ',
+    Case
+	When Count(exclusions) > 0 Then '- Exclude '
+	Else '' End,
+	String_agg(exclusions,', ')) AS Pizza_information
+From pizza_topping_cte
+Group By order_id,pizza_name
+
+--Generate an alphabetically ordered comma separated ingredient list for each pizza order from the customer_orders table and add a 2x in front of any relevant ingredients
+-- This solution is without extras and exclusions.
+With toppings_counts_cte as(       
+Select order_id,pizza_name,topping_name,
+	Count(topping_name) as counts
+From pizza_toppings t,cleaned_customer c 
+Inner Join pizza_recipes r
+On c.pizza_id = r.pizza_id
+Inner Join pizza_names n
+On c.pizza_id = n.pizza_id
+Where topping_id in (
+   Select 
+      UNNEST(string_to_array(r.toppings,',') :: int[])
+      )
+Group By order_id,pizza_name,topping_name)
+
+Select order_id,
+     		Concat(pizza_name,': ',string_agg(Case
+      		When counts > 1 
+      		Then '2x' || topping_name 
+      		Else topping_name End,','))
+From toppings_counts_cte
+Group By order_id,pizza_name 
 
