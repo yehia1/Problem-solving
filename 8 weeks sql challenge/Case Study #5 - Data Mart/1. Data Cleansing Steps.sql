9112e024d02f182c10c608c0da37ad17a0896993
Yehia Hossam <yehiahosam34@gmail.com>
2023-02-26 18:36:38 +0200

    Case study 5 some sql

--- 8 weeks sql challenge/Case Study #5 - Data Mart/1. Data Cleansing Steps.sql
+++ 8 weeks sql challenge/Case Study #5 - Data Mart/1. Data Cleansing Steps.sql
@@ -4,27 +4,27 @@ DROP TABLE IF EXISTS clean_weekly_sales;
 CREATE TEMP TABLE clean_weekly_sales AS (
 SELECT
 	To_date(week_date, 'dd/mm/yy') as date,
-  	Extract('week' from To_date(week_date, 'dd/mm/yy')) as week,
-  	Extract('month' from To_date(week_date, 'dd/mm/yy')) as month,
-  	Extract('year' from To_date(week_date, 'dd/mm/yy')) as year,
+  	Extract('week' From To_date(week_date, 'dd/mm/yy')) as week,
+  	Extract('month' From To_date(week_date, 'dd/mm/yy')) as month,
+  	Extract('year' From To_date(week_date, 'dd/mm/yy')) as year,
   region,
   platform,
   segment,
-case 
-  	when right(segment,1) = '1' then 'Young Adults'
-  	when right(segment,1) = '2' then 'Middle Aged'
-  	when right(segment,1) in ('3','4') then 'Retired'
-  	else 'unknown'
-  	end as age_band,
-case
-  	when left(segment,1) = 'C' then 'Couples'
-  	when left(segment,1) = 'F' then 'Families'
-  	else 'unknown'
-  	end as demographic,
+Case 
+  	When Right(segment,1) = '1' Then 'Young Adults'
+  	When Right(segment,1) = '2' Then 'Middle Aged'
+  	When Right(segment,1) in ('3','4') Then 'Retired'
+  	Else 'unknown'
+  	End as age_band,
+Case
+  	When Left(segment,1) = 'C' Then 'Couples'
+  	When Left(segment,1) = 'F' Then 'Families'
+  	Else 'unknown'
+  	End as demographic,
   customer_type,
   transactions,
   sales,
-Round(sales :: Numeric /transactions :: Numeric ,2)
+  Round(sales :: Numeric /transactions :: Numeric ,2)
   as avg_transaction
 FROM weekly_sales);
   
--- 8 weeks sql challenge/Case Study #5 - Data Mart/2. Data Exploration.sql
+++ 8 weeks sql challenge/Case Study #5 - Data Mart/2. Data Exploration.sql
@@ -1,63 +1,97 @@
 --What day of the week is used for each week_date value?
 Select 
 	To_Char(Date,'Day')
-    from clean_weekly_sales
-    Limit 1;
+From clean_weekly_sales
+Limit 1;
 
 
 -- What range of week numbers are missing from the dataset?
 With weeks as 
 (Select Generate_series(1,52) as week_numbers)
 
-Select * from weeks
-where week_numbers not in 
-(Select week from clean_weekly_sales);
+Select * From weeks
+Where week_numbers not in 
+(Select week From clean_weekly_sales);
 
 --How many total transactions were there for each year in the dataset?
-Select year, count(transactions)
-from clean_weekly_sales 
-group by year
+Select year, Count(transactions)
+From clean_weekly_sales 
+Group By year
 
 -- What is the total sales for each region for each month?
-Select region,month,sum(sales)
-from clean_weekly_sales
-group by region,month
-order by region,month
+Select region,month,Sum(sales)
+From clean_weekly_sales
+Group By region,month
+Order By region,month
 
 -- What is the total count of transactions for each platform
-Select platform,count(transactions)
-from clean_weekly_sales
-group by platform	
-order by platform
+Select platform,Count(transactions)
+From clean_weekly_sales
+Group By platform	
+Order By platform
 
 
 --What is the percentage of sales for Retail vs Shopify for each month?
-
-with platform_monthly_sales as(
-Select month,platform,sum(sales) total_platform_month
-from clean_weekly_sales
-group by month,platform	
-order by month,platform),
+With platform_monthly_sales as(
+  Select month,platform,sum(sales) total_platform_month
+  From clean_weekly_sales
+  Group by month,platform	
+  Order by month,platform),
 
 monthly_sales as 
-(SELECT month,sum(sales) total_month
-from clean_weekly_sales
-group by month
-order by month)
+  (SELECT month,Sum(sales) total_month
+  From clean_weekly_sales
+  Group By month
+  Order By month)
 
 Select m.month,
-	Round(100 *min(case 
-    	when platform = 'Retail'
-        then total_platform_month :: Numeric/ total_month :: Numeric
-    end),2)as retail_percent,
-    Round(100*min(case 
-    	when platform = 'Shopify'
-        then total_platform_month :: Numeric/ total_month :: Numeric
-    end),2) as Shopify_percent
-from monthly_sales m
-inner join platform_monthly_sales p
-on m.month = p.month
-group by 1
+	Round(100 *Min(
+    Case 
+    	When platform = 'Retail'
+        Then total_platform_month :: Numeric/ total_month :: Numeric
+    End),2)as retail_percent,
+  Round(100*Min(
+    Case 
+    	When platform = 'Shopify'
+        Then total_platform_month :: Numeric/ total_month :: Numeric
+    End),2) as Shopify_percent
+From monthly_sales m
+Inner join platform_monthly_sales p
+On m.month = p.month
+Group By 1
+
+--or 
+With Retail_monthly_sales as(
+  Select month,platform,Sum(sales) total_retail_month
+  From clean_weekly_sales
+  Where platform = 'Retail'
+  Group By month,platform	
+  Order By month,platform),
+
+Shopify_monthly_sales as(
+  Select month,platform,Sum(sales) total_shopify_month
+  From clean_weekly_sales
+  Where platform = 'Shopify'
+  Group By month,platform	
+  Order By month,platform),
+
+monthly_sales as (
+  Select month,Sum(sales) total_month
+  From clean_weekly_sales
+  Group By month
+  Order By month)
+
+Select m.month,
+	Round(100 * total_retail_month :: Numeric / total_month,2)
+	as retail_percent,
+    Round(100 * total_shopify_month :: Numeric / total_month,2)
+	as shopify_percent
+From Retail_monthly_sales r
+Inner Join monthly_sales m
+On r.month = m.month
+Inner Join Shopify_monthly_sales s 
+On s.month = m.month
+
 
 
 -- What is the percentage of sales by demographic for each year in the dataset?
