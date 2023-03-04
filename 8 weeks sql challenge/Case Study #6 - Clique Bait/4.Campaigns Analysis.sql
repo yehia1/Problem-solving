541d11622ce4c78188550b8e92d31f889ac71f69
Yehia Hossam <yehiahosam34@gmail.com>
2023-03-04 10:51:23 +0200

    sql 1 day 7

--- 8 weeks sql challenge/Case Study #6 - Clique Bait/4.Campaigns Analysis.sql
+++ 8 weeks sql challenge/Case Study #6 - Clique Bait/4.Campaigns Analysis.sql
@@ -2,21 +2,21 @@ set search_path = clique_bait;
 
 --Generate a table that has 1 single row for every unique visit_id record and has the following columns:
 Select u.user_id,e.visit_id,
-min(e.event_time) as visit_start_time,
-sum(case when e.event_type = 1 then 1 end) as page_views,
-sum(case when e.event_type = 2 then 1 else 0 end) as cart_adds,
-sum(case when e.event_type = 3 then 1 else 0 end) as purchases,
-c.campaign_name,
-sum(case when e.event_type = 4 then 1 else 0 end) as impression,
-sum(case when e.event_type = 5 then 1 else 0 end) as click,
-STRING_AGG(CASE WHEN p.product_id IS NOT NULL AND e.event_type = 2 THEN p.page_name ELSE NULL END, 
-    ', ' ORDER BY e.sequence_number) AS cart_products
-from events e 
-inner join users u
-on u.cookie_id = e.cookie_id
+  Min(e.event_time) as visit_start_time,
+  Sum(Case When e.event_type = 1 Then 1 End) as page_views,
+  Sum(Case When e.event_type = 2 Then 1 Else 0 End) as cart_adds,
+  Sum(Case When e.event_type = 3 Then 1 Else 0 End) as purchases,
+  c.campaign_name,
+  Sum(Case When e.event_type = 4 Then 1 Else 0 End) as impression,
+  Sum(Case When e.event_type = 5 then 1 Else 0 End) as click,
+  STRING_AGG(CASE WHEN p.product_id IS NOT NULL AND e.event_type = 2 THEN p.page_name ELSE NULL END, 
+      ', ' ORDER BY e.sequence_number) AS cart_products
+From events e 
+Inner join users u
+On u.cookie_id = e.cookie_id
 LEFT JOIN campaign_identifier AS c
 ON e.event_time BETWEEN c.start_date AND c.end_date
-LEFT JOIN clique_bait.page_hierarchy AS p
+LEFT JOIN age_hierarchy AS p
   ON e.page_id = p.page_id
-group by 1,2,campaign_name
-order by 1
+Group By 1,2,campaign_name
+Order By 1
