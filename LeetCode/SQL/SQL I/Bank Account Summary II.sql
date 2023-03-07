d0e943e3537b8362e34879d8636d3e4aceb45e23
Yehia Hossam <yehiahosam34@gmail.com>
2023-03-07 15:36:48 +0200

    Sql 1 finished

---
+++ LeetCode/SQL/SQL I/Bank Account Summary II.sql
@@ -0,0 +1,24 @@
+-- https://leetcode.com/problems/bank-account-summary-ii/description/?envType=study-plan&id=sql-i
+
+Create table If Not Exists Users (account int, name varchar(20))
+Create table If Not Exists Transactions (trans_id int, account int, amount int, transacted_on date)
+Truncate table Users
+insert into Users (account, name) values ('900001', 'Alice')
+insert into Users (account, name) values ('900002', 'Bob')
+insert into Users (account, name) values ('900003', 'Charlie')
+Truncate table Transactions
+insert into Transactions (trans_id, account, amount, transacted_on) values ('1', '900001', '7000', '2020-08-01')
+insert into Transactions (trans_id, account, amount, transacted_on) values ('2', '900001', '7000', '2020-09-01')
+insert into Transactions (trans_id, account, amount, transacted_on) values ('3', '900001', '-3000', '2020-09-02')
+insert into Transactions (trans_id, account, amount, transacted_on) values ('4', '900002', '1000', '2020-09-12')
+insert into Transactions (trans_id, account, amount, transacted_on) values ('5', '900003', '6000', '2020-08-07')
+insert into Transactions (trans_id, account, amount, transacted_on) values ('6', '900003', '6000', '2020-09-07')
+insert into Transactions (trans_id, account, amount, transacted_on) values ('7', '900003', '-4000', '2020-09-11')
+
+-- Write your MySQL query statement below
+Select name,sum(amount) as balance
+From Transactions t
+Inner Join Users u
+On t.account = u.account
+Group By name
+Having sum(amount) > 10000;
