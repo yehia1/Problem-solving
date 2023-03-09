cc5ad9fc827a6e95a86a17a597cdd0b5d8c0f0c2
Yehia Hossam <yehiahosam34@gmail.com>
2023-03-09 21:16:26 +0200

    middle of linked list

--- LeetCode/Algorithms 1/Day5/Middle-of-the-linked-list.py
+++ LeetCode/Algorithms 1/Day5/Middle-of-the-linked-list.py
@@ -12,3 +12,13 @@ class Solution:
                 head2 = head2.next
             # print(head2)
             return head2
+    
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        list1 = []
+
+        length = 0
+        while(head != None):
+            list1.append(head)
+            head = head.next
+            length +=1
+        return (list1[length // 2])
