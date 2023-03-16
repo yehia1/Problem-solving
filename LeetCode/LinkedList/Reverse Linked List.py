7a45c86d66139a3963f1e96d6a1a8e13d7edcc09
Yehia Hossam <yehiahosam34@gmail.com>
2023-03-16 22:51:25 +0200

    reverse a linked list

---
+++ LeetCode/LinkedList/Reverse Linked List.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/reverse-linked-list/description/
+
+'''
+Given the `head` of a singly linked list, reverse the list, and return the reversed list.
+
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        while(curr):
+            _next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = _next
+        
+        return prev
+
+
+        
