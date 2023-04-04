6fdae646500a9cc3067318b79f24d068a7720a54
Yehia Hossam <yehiahosam34@gmail.com>
2023-04-04 06:28:09 +0200

    Add Binary 2

--- LeetCode/Random/Add Binary.py
+++ LeetCode/Random/Add Binary.py
@@ -6,4 +6,26 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         c = int(a,2) + int(b,2)
         return bin(c).replace('0b','')
-        
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        c = self.BinarytoDecimal(a) + self.BinarytoDecimal(b)
+        return self.DecToBin(c)
+    
+    
+    def BinarytoDecimal(self,n):
+        decimal, i = 0,0
+        n = int(n)
+        while(n !=0):
+            dec = n % 10
+            decimal = decimal + dec * pow(2,i)
+            n = n // 10
+            i += 1
+        return decimal
+
+    def DecToBin(self,num):
+        binary = ""
+        while num > 0:
+            binary = str(num % 2) + binary
+            num //= 2
+        return binary
