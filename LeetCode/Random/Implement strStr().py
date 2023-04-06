4811ec7b320b130074e4f3ae39c597919e9c4db8
Yehia Hossam <yehiahosam34@gmail.com>
2023-04-07 00:10:19 +0200

    Implement strStr() 2nd solution

--- LeetCode/Random/Implement strStr().py
+++ LeetCode/Random/Implement strStr().py
@@ -6,4 +6,23 @@ or -1 if needle is not part of haystack.
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         return haystack.find(needle)
+
+#another soluton
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        length = len(needle)
+        flag = 0
+        for i in range(len(haystack)):
+            for j in range(length):
+                flag = 0
+                if(i + j == len(haystack)):
+                    flag = 1
+                    break
+                if(haystack[i+j] != needle[j]):
+                    flag = 1
+                    break
+            if(flag == 0):
+                return i
+        return -1
+        
         
