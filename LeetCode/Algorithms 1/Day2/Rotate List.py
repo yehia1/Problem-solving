dc3e5aacd19127d5eccee7499777b1f7c3287c95
Yehia Hossam <yehiahosam34@gmail.com>
2023-04-17 00:34:18 +0200

    Rotate array

--- LeetCode/Algorithms 1/Day2/Rotate List.py
+++ LeetCode/Algorithms 1/Day2/Rotate List.py
@@ -10,3 +10,12 @@ class Solution:
                for i, x in enumerate(nums)]
         for i in range(len(nums)):
             nums[i] = testcase[i]
+
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        result = [0] * n
+        for i in range(n):
+            result[(i+k) % n] = nums[i]
+            
+        for i in range(n):
+            nums[i] = result[i]
